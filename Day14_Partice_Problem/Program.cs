0950e1745534c82b7d56a6bde6b5438746b18dea
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-21 17:35:46 +0530

    Added Welcome Msg

---
+++ Day14_Partice_Problem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Partice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Linked List 56>30>70");
+            Console.ReadKey();
+        }
+    }
+}
