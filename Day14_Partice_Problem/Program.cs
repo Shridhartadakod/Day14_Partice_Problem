6605cd094e76087faeb1c36fb741b8519e06525f
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-21 18:14:54 +0530

    Adding Link List

--- Day14_Partice_Problem/Program.cs
+++ Day14_Partice_Problem/Program.cs
@@ -10,8 +10,13 @@ namespace Day14_Partice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Simple Linked List 56>30>70");
-            Console.ReadKey();
+            UnOrderedLinkedList<int> list=new UnOrderedLinkedList<int>();
+            Console.WriteLine("Adding 56,30,70");
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            Console.WriteLine("list_1");
+            list.Display();
         }
     }
 }
---
+++ Day14_Partice_Problem/UnOrderedLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Partice_Problem
+{
+    internal class UnOrderedLinkedList<T> where T:IComparable<T>
+    {
+        public Node<T> head;
+        public UnOrderedLinkedList()
+        {
+            head = null;
+        }
+        public UnOrderedLinkedList<T> List()
+        {
+            UnOrderedLinkedList<T> emptyList=new UnOrderedLinkedList<T>();
+            return emptyList;
+        }
+        public void Append(T data)
+        {
+            Node<T> node =new Node<T>(data);
+            if(head == null)
+            {
+                head = node;    
+            }
+            else
+            {
+                Node<T> temp = head;
+                while(temp.next!= null)
+                    temp=temp.next;
+                temp.next=node;
+            }
+            
+        }
+        public void Display()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(" " + temp.data);
+                temp = temp.next;
+            }
+        }
+
+    }
+}
