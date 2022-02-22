f224f33501c81ea057885fec8882cf49431fb057
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-22 18:07:15 +0530

    Stack is Created

---
+++ Day14_Partice_Problem/StackList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Partice_Problem
+{
+    internal class StackList<T>:LinkedList<T> where T:IComparable
+    {
+        public static StackList<T> Stack()
+        {
+            return new StackList<T>();
+        }
+
+        public void Push(T data)
+        {
+            Add (data);
+        }
+    }
+}
